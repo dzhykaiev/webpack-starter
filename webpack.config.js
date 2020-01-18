6b36652d15de5a0fcc0b442a2e9cfd7b7cb01e84
dzhykaiev <yevhenii.dzhykaiev@gmail.com>
2020-01-18 23:22:30 +0200

    added webpack.config file with settings

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+
+// TODO: add copy paste (fonts, imgs)
+
+module.exports = (env, argv) => {
+  const devMode = argv.mode !== "production";
+  return {
+    entry: {
+      main: "./src/index.js"
+    },
+    devtool: devMode ? "inline-source-map" : false,
+    devServer: {
+      contentBase: "./dist"
+    },
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [
+            devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+            "css-loader",
+            "sass-loader"
+          ]
+        }
+      ]
+    },
+    plugins: [
+      new MiniCssExtractPlugin({
+        filename: "[name].css",
+        chunkFilename: "[id].css"
+      }),
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: "./src/index.html",
+        hash: true
+      })
+    ],
+    output: {
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist")
+    },
+    optimization: {
+      minimizer: [
+        new TerserJSPlugin(),
+        new OptimizeCSSAssetsPlugin(),
+        new HtmlWebpackPlugin({
+          minify: {
+            removeAttributeQuotes: true,
+            collapseWhitespace: true,
+            removeComments: true
+          }
+        })
+      ]
+    }
+  };
+};
